395f4c46bd3f5bfc0c21784abc8a7712627bc67d
Yousef Amar <yousefamar@gmail.com>
2017-02-27 13:12:07 +0000

    Generate maven project

---
+++ src/main/java/uk/databoxproject/app-template/App.java
@@ -0,0 +1,11 @@
+package uk.databoxproject.databox-app-template-java;
+
+/**
+ * Example app
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
